49e71d0cd9b3347c6644f75e28754e10184f116f
rightgo09 <skyarrow09@gmail.com>
2012-12-18 19:10:22 +0900

    rails new RoR-MyTemplate -d mysql -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRMytemplate::Application.config.secret_token = '54b8a9812305c3f700aeb16dc5d9b56576ce0210ee72f8ee8d2ad01512b5c9bb5146853f98b120414cabcb33521529020593d8cb5a0cc3c9288f15e35c76612c'
